b7e5d0ae68b942c04ba8d680b20fb4fb84d525e4
Harry Berg <harryaberg@gmail.com>
2020-10-15 14:03:40 +0100

    added link to my profile to download VOC

--- Chapter 4. Deep Learning/Module 8. Computer Vision basics/VOCDataset.py
+++ Chapter 4. Deep Learning/Module 8. Computer Vision basics/VOCDataset.py
@@ -8,14 +8,15 @@ label_transform = transforms.Compose([
 ])
 
 class VOC(torch.utils.data.Dataset):
-    def __init__(self, input_transform=None):
-        self.filenames = [fn.split('.')[0] for fn in os.listdir('./VOC2007/SegmentationClass')]
+    def __init__(self, root, input_transform=None):
+        self.root = root
+        self.filenames = [fn.split('.')[0] for fn in os.listdir(f'{self.root}/SegmentationClass')]
         self.input_transform = input_transform
 
     def __getitem__(self, idx):
         fn = self.filenames[idx]
-        img = Image.open(f'./VOC2007/JPEGImages/{fn}.jpg')
-        label = Image.open(f'./VOC2007/SegmentationClass/{fn}.png')
+        img = Image.open(f'{self.root}/JPEGImages/{fn}.jpg')
+        label = Image.open(f'{self.root}/SegmentationClass/{fn}.png')
 
         if self.input_transform:
             img = self.input_transform(img)
